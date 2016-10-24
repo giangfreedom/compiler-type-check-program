5e57384e8f75b7b144cdcd82c7c45ebd50c6b5ce
giangfreedom <giangtruong321@yahoo.com>
2016-10-23 20:02:53 -0700

    update
    add more pattern

--- PatternMatching.java
+++ PatternMatching.java
@@ -4,33 +4,61 @@ import java.util.regex.Pattern;
 
 public class PatternMatching {
 	// a single char lower or upper case
-	private static final String c = "[a-zA-z]+";
-	// a single number 0-9
+	private static final String c = "[a-zA-z]";
+	// a number between 0-9 
 	private static final String n = "[0-9]";
-	   
-	private static final String patternPrimitive = ("int|double|char|float|long|short|bool");
 	
-	private static final Pattern primitive = Pattern.compile(patternPrimitive);
-	   
-	private static final String patternPrimitiveArray = ("int[]|double[]|char[]|float[]|long[]|short[]");
-	   
-	private static final String patternReturntype = ("int|double|char|float|long|short|bool|void");
-	    
-	//private static final String patternmain = patternPrimitive + " main()";
+	// operator +/-/*/ /
+	private static final Pattern patternOperator = Pattern.compile("[+|-|*|/]");
 	
+	//  true false boolean
+	private static final Pattern patternBoolean = Pattern.compile("true|false");
+	
+	// left parathesis (
+	private static final Pattern patternLParen = Pattern.compile("[(]");
+	// right parathesis )
+	private static final Pattern patternRParen = Pattern.compile("[)]");
+	// comparasion
+	private static final Pattern patternComparasion = Pattern.compile("<|>|<=|>=|==");
+	//assignment
+	private static final Pattern patternAssignment = Pattern.compile("=");
+	// and or not
+	private static final Pattern patternChoice = Pattern.compile("&&| || |!");
+	// conditional
+	private static final Pattern patternCondition = Pattern.compile("if|else if|while|do|for");
+	
+	private static final Pattern patternElse= Pattern.compile("else");
+	
+	private static final Pattern patternBreak= Pattern.compile("break");
+	private static final Pattern patternCont= Pattern.compile("continue");
+	
+	// end of file
+	private static final Pattern patternEOF= Pattern.compile("EOF");
+	
+	   
+	//private static final String patternPrimitive = ("int|double|char|float|long|short|bool");
+	
+	private static final Pattern primitive = Pattern.compile("(^)int($)|(^)double($)|(^)char($)|(^)float($)|(^)long($)|(^)short($)|(^)bool($)");
+	   
+	private static final String patternPrimitiveArray = ("(^)int[]($)|(^)double[]($)|(^)char[]($)|(^)float[]($)|(^)long[]($)|(^)short[]($)");
+	   
+	private static final String patternReturntype = ("(^)int($)|(^)double($)|(^)char($)|(^)float($)|(^)long($)|(^)short($)|(^)bool($)|(^)void($)");
+	    	
 	private static final String patternmain = "int main()";		
 
 	private static final Pattern main = Pattern.compile(patternmain);
 	
-	private static final String patternfuncname = c;
+	private static final String patternfuncname = "^" + c + "[a-zA-z0-9]{1,14}" +"$";
 	
-	private static final String patternvariablename = "^" + c + "[a-zA-z0-9]*" +"$";
+	private static final String patternvariablename = "^"+c + "[a-zA-z0-9]{1,14}$";
 	private static final Pattern variable = Pattern.compile(patternvariablename);
 	
-	private static final String patternparameter = primitive + patternvariablename +"$";
+	
+	private static final String patternparameter = "[(^)int|(^)double|(^)char|(^)float|(^)long|(^)short|(^)bool]\\s[a-zA-z][a-zA-z0-9]{1,14}$";
 	private static final Pattern parameter = Pattern.compile(patternparameter);
 	
-	private static final String patternfuncheader = patternReturntype +"\\s"+ patternfuncname +"\\s(" + patternparameter + ")$";
+	private static final String patternfuncreturntype = ("[(^)int|(^)double|(^)char|(^)float|(^)long|(^)short|(^)bool|(^)void]");
+	private static final String patternfuncheader = "[(^)int|(^)double|(^)char|(^)float|(^)long|(^)short|(^)bool|(^)void]\\s[a-zA-z][a-zA-z0-9]{1,14}\\s" + patternparameter;
 	
 	private static final Pattern funcheader = Pattern.compile(patternfuncheader);
 	
--- patternTesting.java
+++ patternTesting.java
@@ -7,20 +7,24 @@ public class patternTesting {
 		PatternMatching mycheck = new PatternMatching();
 		String m = "int main()";
 		String m1 = "double myfunc1 (int myvalue1)";
-		String m2 = "int";
-		String m3 = "short myvalue int";
-		
+		String m2 = "double";
+		String m3 = "int dg1dfgd";
+		String m4 = "m1yval23kjhfghj";
+		/*
 		if(mycheck.visitmain(m)){
 			System.out.println("int main() passed");
 		}
 		if(mycheck.visitfunction(m1)){
 			System.out.println("double myfunc1 passed");
-		}
+		}*/
 		if(mycheck.visitPrimitive(m2)){
-			System.out.println("int passed");
+			System.out.println("double passed");
 		}
 		if(mycheck.visitParam(m3)){
 			System.out.println("param passed");
+		}
+		if(mycheck.visitVariable(m4)){
+			System.out.println("variable name passed");
 		}
 	}
 
