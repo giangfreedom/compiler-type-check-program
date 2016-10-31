d6832d32b20ea58804fc1f0452416c1336c13c6e
giangfreedom <giangtruong321@yahoo.com>
2016-10-30 19:54:25 -0700

    sunday update

--- PatternMatching.java
+++ PatternMatching.java
@@ -42,25 +42,48 @@ public class PatternMatching {
 	   
 	private static final String patternPrimitiveArray = ("(^)int[]($)|(^)double[]($)|(^)char[]($)|(^)float[]($)|(^)long[]($)|(^)short[]($)");
 	   
-	private static final String patternReturntype = ("(^)int($)|(^)double($)|(^)char($)|(^)float($)|(^)long($)|(^)short($)|(^)bool($)|(^)void($)");
+	private static final Pattern Returntype = Pattern.compile("(^)int($)|(^)double($)|(^)char($)|(^)float($)|(^)long($)|(^)short($)|(^)bool($)|(^)void($)");
 	    	
 	private static final String patternmain = "int main()";		
 
 	private static final Pattern main = Pattern.compile(patternmain);
 	
-	private static final String patternfuncname = "^" + c + "[a-zA-z0-9]{1,14}" +"$";
+	private static final String patternfuncname = c + "[a-zA-z0-9]{1,14}";
 	
 	private static final String patternvariablename = "^"+c + "[a-zA-z0-9]{1,14}$";
 	private static final Pattern variable = Pattern.compile(patternvariablename);
 	
 	
-	private static final String patternparameter = "[(^)int|(^)double|(^)char|(^)float|(^)long|(^)short|(^)bool]\\s[a-zA-z][a-zA-z0-9]{1,14}$";
+	private static final String patternparameter = "([int|double|char|float|long|short|bool]\\s[a-zA-z][a-zA-z0-9]{1,14}){1,14}";
 	private static final Pattern parameter = Pattern.compile(patternparameter);
 	
 	private static final String patternfuncreturntype = ("[(^)int|(^)double|(^)char|(^)float|(^)long|(^)short|(^)bool|(^)void]");
-	private static final String patternfuncheader = "[(^)int|(^)double|(^)char|(^)float|(^)long|(^)short|(^)bool|(^)void]\\s[a-zA-z][a-zA-z0-9]{1,14}\\s" + patternparameter;
+	private static final String patternfuncheader = "^" + "[int|double|char|float|long|short|bool|void]\\s[a-zA-z][a-zA-z0-9]{1,14}\\s(\\s" + patternparameter +"\\s)$";
 	
 	private static final Pattern funcheader = Pattern.compile(patternfuncheader);
+	
+	private static final Pattern funcCallTwoArgument = Pattern.compile("^"+patternfuncname + "(" + patternvariablename +",\\s"+ patternvariablename + ");$");
+	
+	
+	// NOTE we won't have " int a = 1+2 " or "int a = a+b" a+# or #+b or a+b+c or etc...
+	// WE WILL ONLY HAVE THE BASE CASE int a; or int a = #;
+	private static final Pattern varNumberVer1 = Pattern.compile("^" + "[int|double|float|long|short]\\s[a-zA-z][a-zA-z0-9]{0,14}"+";$");
+	private static final Pattern varnumberVer2 = Pattern.compile("^" + "[int|double|float|long|short]\\s[a-zA-z][a-zA-z0-9]{0,14}\\s"+ "=\\s[0-9]{1,14}"+";$");
+	private static final Pattern varCharVer1 = Pattern.compile("^" + "char\\s[a-zA-z][a-zA-z0-9]{0,14}"+";$");
+	private static final Pattern varCharVer2 = Pattern.compile("^" + "char\\s[a-zA-z][a-zA-z0-9]{0,14}\\s"+ "=\\s"+"'"+"[a-zA-z]"+"'"+";$");
+	private static final Pattern varBoolVer1 = Pattern.compile("^" + "bool\\s[a-zA-z][a-zA-z0-9]{0,14}"+";$");
+	private static final Pattern varBoolVer2 = Pattern.compile("^" + "bool\\s[a-zA-z][a-zA-z0-9]{0,14}\\s"+ "=\\s[true|false]"+";$");
+
+	//private static final Pattern variableDeclation = Pattern.compile("[var1|var2]"));
+	public static boolean visitVariableDeclaration (String target) {
+		return (varNumberVer1.matcher(target).find() || varnumberVer2.matcher(target).find()
+				|| varCharVer1.matcher(target).find() || varCharVer2.matcher(target).find()
+				|| varBoolVer1.matcher(target).find() || varBoolVer2.matcher(target).find());
+	}
+	
+	public static boolean visitFunctionCall (String target) {
+		return funcCallTwoArgument.matcher(target).find();
+	}
 	
 	public static boolean visitVariable (String target) {
 		return variable.matcher(target).find();
@@ -72,6 +95,10 @@ public class PatternMatching {
 	
 	public static boolean visitPrimitive (String target) {
 		return primitive.matcher(target).find();
+	}
+	
+	public static boolean visitReturnType (String target) {
+		return Returntype.matcher(target).find();
 	}
        
    	public static boolean visitmain (String target) {
--- typecheck.java
+++ typecheck.java
@@ -1,5 +1,7 @@
 package simpleTypeCheck;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
@@ -10,8 +12,33 @@ public class typecheck {
 	private static int openbracecount = 0;
 	private static int closebracecount = 0;
 	private static int maincount = 0;
+	private static int functioncount = 0;
+	private static int returncount = 0;
 	
-	private HashMap<String, String[]> mIndex;
+	// hash map that store the function info
+	// key is function name, value is an arraylist
+	// 1st item in arraylist is function return typr
+	// 2nd item is function unique id
+	// 3rd is function param count
+	// 4th param return type, 5th param name
+	// 6th param return type, 7th param name
+	// caculator add/minus/mul/ div functions only have 2 param.
+	private static HashMap<String, ArrayList<String>> mFunctionIndex;
+	
+	// hashmap that store variable info
+	// key is variable name, value is the return type
+	private static HashMap<String, String> mVariableIndex;
+	
+	// hash map that store return info
+	// key is the number of return start at 1 increase by 1 
+	// for every return we encounter, value is the variable
+	// name that we are returning, use it to access the
+	// variable hashmap to obtain the type of the variable.
+	// the key (number) match the function unique ID,
+	// function unique ID = 1 mean it is linked to return
+	// key = 1
+	private static HashMap<Integer, String> mReturnIndex;
+	
 	/*
 	public typecheck(String input){
 		mystring = input;
@@ -19,7 +46,7 @@ public class typecheck {
 	
 	// remove all comma, semi colon
 	public static String RemoveAllCommaNSemicolon(String in){
-		in = in.replace(",", "");
+		in = in.replace(",", " ");
 		in = in.replace(";", "");
 		in = in.trim();
 		return in;
@@ -45,48 +72,100 @@ public class typecheck {
 			input.replace("}", "");
 			input = input.trim();
 		}
-		// check to make sure we only have 1 main
-		if(input.contains("main()")){
-			maincount++;
-			if(maincount > 1){
-				System.out.println("error code 1");
-			}
-		}
 		// split the string into array of string
 		String[] arr = input.split("\\s+");
-		// the string begin with a primitive type
-		if(mycheck.visitPrimitive(arr[0])){
-			// match the string against pattern
-			
-			// match against main pattern
-			// output accepted 1 if it pass
-			// int main()
-			if(mycheck.visitmain(input)){
-				System.out.println("accepted 1");
-			}
-			// match against func declaration
-			// exp: int myfunc (int xyz) 
-			else if(mycheck.visitfunction(input)){
-				System.out.println("accepted 2");
-			}
-			// match against variable declaration
-			// int a = 1;
-			
-			// int a;						
-		}
-		// the string start with a variable name
-		else if(mycheck.visitVariable(input)){
-			// a = 1 + 1;
-			// a = 5;
-			// a + b = c + d;
-		}
-		// start with return 
 		
-		// start with function name follow by ()
-		// for this one we need to save function name in
-		// the hashmap every time we matched a function pattern
-		// so we can remember the function declation/param/returntype
-		// for futher checking
+		// check to make sure we only have 1 main
+		// and main pass the pattern int main()
+		if(input.contains("main()")){
+			maincheck(input, mycheck);
+		}
+		// check function pattern
+		else if(mycheck.visitfunction(input)){
+			// check to see if the function name (procedure ID)
+			// already appear in the hashmap
+			// if it does error 3 no duplicate procedure ID allowed
+			// get key from mFunctionIndex and compare to arr[1](the func name)
+			if(isFunctionNameDuplicate(arr[1])){
+				// we found duplicate error 3
+				System.out.println("error 3");
+			}
+			// no duplicated name found
+			// populate the function hashmap with the function information
+			else{
+				functionPopulate(input);
+				System.out.println("function header passed");
+			}
+		}
+		// check variable declaration pattern
+		else if(mycheck.visitVariableDeclaration(input)){
+			// check for variable duplicate
+			if(isVariableNameDuplicate(arr[1])){
+				// we found duplicate error 3
+				System.out.println("error 4 variable name duplicate");
+			}
+			else{
+				// get the variable name and data type
+				// save them in the variable hashmap
+				// arr[1] var name, arr[0] var type
+				mVariableIndex.put(arr[1], arr[0]);
+				System.out.println("variable declaration passed");
+			}
+		}
+		// check function call
+		else if(mycheck.visitFunctionCall(input)){
+			boolean pass = true;
+			String s = input;
+			int j = 4;
+			s = RemoveAllCommaNSemicolon(s);
+			s = s.replaceAll("(", " ");
+			s = s.replaceAll(")", "");
+			String sarr[] = s.split(" ");
+			// 1st item in array is function name
+			// go into the function hashmap to check for it existence
+			if(!isFunctionNameDuplicate(sarr[0])){
+				// no duplicate = no found = function is not declared before it is call
+				System.out.println("error 5 function name not found in hashmap");
+				pass = false;
+			}
+			// check for number or argument and param
+			// access the function hashmap get the value (arraylist) using
+			// the key (function name), then get the 3rd index from array list
+			// for the param number (count) compare it against number 2 (because
+			// our caculator only have function using 2 argument/param
+			else if(!(Integer.parseInt(mFunctionIndex.get(sarr[0]).get(2).trim()) == 2)){
+				System.out.println("error 6 miss match argument/param number");
+				pass = false;
+			}
+			else{
+				// check argument type error , since argument is variable
+				// go to variable hashmap grab the value (the type)
+				// compare to the function hashmap param type.
+				for(int i = 0; i < sarr.length-1; i++){
+					// go to variable hashmap grab the value (the type) using sarr[i+1] the key
+					// compare to the function hashmap param type starting at arraylist 4th index increase by 2 each loop.
+					if(!(mVariableIndex.get(sarr[i+1]).equals(mFunctionIndex.get(sarr[0]).get(j)))){
+						System.out.println("error 7 data type miss match argument/param");
+						pass = false;
+						break;
+					}
+					j = j+2;
+				}
+			}
+			if(pass == true){
+				System.out.println("function call passed");
+			}
+		}
+		// check return eror code 8 
+		// else if (return stuff)
+
+		// check a = add();
+		// function return and var type error 9
+		
+		// check if error 10
+		
+		// check while error 11
+		
 		
 		// if we get here the line failed all matching
 		else{
@@ -94,5 +173,144 @@ public class typecheck {
 		}
 			
 	}
-
+	
+	public static void maincheck(String input, PatternMatching mycheck){
+		maincount++;
+		// case of more than 1 main() appear
+		if(maincount > 1){
+			System.out.println("error code 1");
+		}
+		// case of int main() fail the pattern matching
+		else if(!mycheck.visitmain(input)){
+			System.out.println("error code 2");
+			System.out.println("main fail pattern matching not neccessary main with argument");
+		}
+		else{
+			System.out.println("main passed");
+		}		
+	}
+	
+	public static void functionPopulate(String input){
+		// split the input using split function
+		// add each item in array into the hashmap 
+		// the function name will be the key
+		// value is an arraylist of return type, function unique ID
+		// param number(count), 1st param return type, 1st param name, 
+		// 2nd param return type, 2nd param name etc...
+		// split the string into array of string using space between word
+		String[] arr = input.split("\\s+");
+		ArrayList<String> value = new ArrayList<String>();
+		
+		// add the return type first
+		value.add(arr[0]);
+		
+		// add the unique id, this id is used
+		// to match it against return type hashmap
+		value.add(""+functioncount);			
+		// update function count by +1
+		functioncount++;
+		
+		// add number of parameter (param count of the function)
+		// the size of the arr[] - 4 (return type, name, open paren, close paren)
+		value.add(""+((arr.length - 4)/2));
+		
+		// we have 1 or more param
+		if(!((arr.length - 4)/2 == 0)){
+			// loop through the arr[] add all param
+			// return type and name into the value arraylist
+			for (int i = 0; i < arr.length; i++){
+				if(arr[i].equalsIgnoreCase("(")){
+					// add param return type and name intp value list
+					for(int j = i+1; j < arr.length; j= j+2){
+						if(!arr[j].equalsIgnoreCase(")")){
+							// param return type
+							value.add(arr[j]);
+							// param name
+							value.add(arr[j+1]);
+						}
+					}
+					break;
+				}
+			}
+		}
+		
+		mFunctionIndex.put(arr[1], value);		
+	}
+	
+	// loop throught the list of key(function name)  in mFunctionIndex
+	// return true if found duplicate, false if there is no duplicate
+	public static boolean isFunctionNameDuplicate(String functionName) {
+		// we have nothing in the hashmap mean no function was added at all
+		// no duplicate possible
+		if(mFunctionIndex.isEmpty() == true){
+			return false;
+		}
+		// we have at least 1 function added
+		else{
+			String[] funcnameList = getFunctionDictionary();
+			// loop the whole list of function name
+			for(int i = 0; i < funcnameList.length; i++){
+				// if found an equal (duplicated) return false
+				if(functionName.equals(funcnameList[i])){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	public static String[] getFunctionDictionary() {
+		// TO-DO: fill an array of Strings with all the keys from the hashtable.
+		// Sort the array and return it.
+		List<String> keys = new ArrayList<String>();
+		for ( String key : mFunctionIndex.keySet() ) {
+			keys.add(key);
+		}
+			  
+		String [] mystrarr = new String[keys.size()];
+		for(int j = 0; j < mystrarr.length; j++){
+			mystrarr[j] = keys.get(j);
+		}
+			  
+		return mystrarr;
+	}
+	
+	// loop throught the list of key(variable name)  in mVariableIndex
+	// return true if found duplicate, false if there is no duplicate
+	public static boolean isVariableNameDuplicate(String variableName) {
+		// we have nothing in the hashmap mean no variable was added at all
+		// no duplicate possible
+		if(mVariableIndex.isEmpty() == true){
+			return false;
+		}
+		// we have at least 1 variable added
+		else{
+			String[] variableList = getVariableDictionary();
+			// loop the whole list of variable name
+			for(int i = 0; i < variableList.length; i++){
+				// if found an equal (duplicated) return false
+				if(variableName.equals(variableList[i])){
+					return true;
+				}
+			}
+		}
+		return false;
+	}	
+	
+	public static String[] getVariableDictionary() {
+		// TO-DO: fill an array of Strings with all the keys from the hashtable.
+		// Sort the array and return it.
+		List<String> keys = new ArrayList<String>();
+		for ( String key : mVariableIndex.keySet() ) {
+			keys.add(key);
+		}
+			  
+		String [] mystrarr = new String[keys.size()];
+		for(int j = 0; j < mystrarr.length; j++){
+			mystrarr[j] = keys.get(j);
+		}
+			  
+		return mystrarr;
+	}	
+	
 }
